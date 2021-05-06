80dc56a9388d9e15e1ac00603897d6bf54458c29
sa <psa5013@hanmail.com>
2021-05-06 18:13:17 +0900

    userState 선언

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 function App() {
+  // state선언 : HTTP통신을 통해 가져온 배열을ㄹ 저장할 state를 선언.
+  // posts의 초기값 : 빈 배열
+  const [posts, setPosts] = useState([]);           
   return <Container>App</Container>;
 }
 
